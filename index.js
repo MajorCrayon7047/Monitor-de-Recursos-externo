9b136b2024cffb1acd248bc0203aa7fe35bc41e0
MajorCrayon7047 <110138925+MajorCrayon7047@users.noreply.github.com>
2023-02-08 12:20:36 -0300

    Pruebas con serial

---
+++ index.js
@@ -0,0 +1,14 @@
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline')
+
+const port = new SerialPort({ path: 'COM3', baudRate: 9600 });
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+
+// Read the port data
+port.on("open", () => {
+    console.log('serial port open');
+});
+
+parser.on('data', data =>{
+    console.log('Atte Arduino:', data);
+});
